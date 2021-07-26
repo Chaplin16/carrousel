2ee39f5f3bdbdc611c56cc79bd4fcd9b2a68b7a8
Chaplin16 <chaplin16@orange.fr>
2021-07-26 22:56:32 +0200

    4 images avec hover ok

--- js/index.js
+++ js/index.js
@@ -26,6 +26,18 @@ window.onscroll = function () {
             document.getElementById("link4").style.color = "rgb(228, 172, 126)";
         });
 
+        link1.addEventListener("mouseout", function() {
+            document.getElementById("link1").style.color = "white";
+        });
+        link2.addEventListener("mouseout", function() {
+            document.getElementById("link2").style.color ="white";
+        });
+        link3.addEventListener("mouseout", function() {
+            document.getElementById("link3").style.color = "white";
+        });
+        link4.addEventListener("mouseout", function() {
+            document.getElementById("link4").style.color = "white";
+        });
     }
     else{
         document.getElementById("navbar").style.background = "white";
@@ -48,39 +60,59 @@ window.onscroll = function () {
         link4.addEventListener("mouseover", function() {
             document.getElementById("link4").style.color = "brown";
         });
+        
+
+        link1.addEventListener("mouseout", function() {
+            document.getElementById("link1").style.color = "rgb(70, 62, 59)";
+        });
+        link2.addEventListener("mouseout", function() {
+            document.getElementById("link2").style.color = "rgb(70, 62, 59)";
+        });
+        link3.addEventListener("mouseout", function() {
+            document.getElementById("link3").style.color ="rgb(70, 62, 59)";
+        });
+        link4.addEventListener("mouseout", function() {
+            document.getElementById("link4").style.color = "rgb(70, 62, 59)";
+        });
     }
 }
 
 document.getElementById("card1").addEventListener("mouseout", function() {
     document.getElementById("link_card1").style.display = "none";
+    document.getElementById("img_card1").style.filter ="brightness(100%)";
 });
 
 document.getElementById("card2").addEventListener("mouseout", function() {
     document.getElementById("link_card2").style.display = "none";
+    document.getElementById("img_card2").style.filter ="brightness(100%)";
 });
 
 document.getElementById("card3").addEventListener("mouseout", function() {
     document.getElementById("link_card3").style.display = "none";
+    document.getElementById("img_card3").style.filter ="brightness(100%)";
 });
 
 document.getElementById("card4").addEventListener("mouseout", function() {
     document.getElementById("link_card4").style.display = "none";
-    document.getElementById("card4").style.filter ="brightness(100%)";
+    document.getElementById("img_card4").style.filter ="brightness(100%)";
 });
 
 document.getElementById("card1").addEventListener("mouseover", function() {
     document.getElementById("link_card1").style.display = "block";
+    document.getElementById("img_card1").style.filter ="brightness(50%)";
 });
 
 document.getElementById("card2").addEventListener("mouseover", function() {
     document.getElementById("link_card2").style.display = "block";
+    document.getElementById("img_card2").style.filter ="brightness(50%)";
 });
 
 document.getElementById("card3").addEventListener("mouseover", function() {
     document.getElementById("link_card3").style.display = "block";
+    document.getElementById("img_card3").style.filter ="brightness(50%)";
 });
 
 document.getElementById("card4").addEventListener("mouseover", function() {
     document.getElementById("link_card4").style.display = "block";
-    document.getElementById("card4").style.filter ="brightness(50%)";
+    document.getElementById("img_card4").style.filter ="brightness(50%)";
 });
